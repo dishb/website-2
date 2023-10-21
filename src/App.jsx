e1396e0e3be6fcb8aecda0962fd71afb4f2fed92
dishb <code.dishb@gmail.com>
2023-10-20 19:03:01 -0700

    edit: revert last commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Projects from './pages/Projects';
 
 function App () {
     return (
-        <BrowserRouter basename = {process.env.PUBLIC_URL}>
+        <BrowserRouter>
             <Routes>
                 <Route path = '/' element = {<Home />} />
                 <Route path = '/about' element = {<About />} />
