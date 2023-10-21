3a9c348ea6089311ebb756e7dc279dcf952a781b
dishb <code.dishb@gmail.com>
2023-10-21 10:26:39 -0700

    feat: make bg blurred for theme icon

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -15,9 +15,11 @@ function toggleMode () {
 
 function Theme () {
     return (
-        <button className = 'm-5 fixed'>
-            <i id = 'theme-icon' onClick = {toggleMode} className = 'fa-solid fa-moon text-2xl text-accent hover:text-hover dark:text-darkaccent dark:hover:text-darkhover' />
-        </button>
+        <div className = 'bg-bg dark:bg-darkbg fixed w-screen bg-opacity-80 backdrop-blur-sm dark:bg-opacity-60'>
+            <button className = 'm-5'>
+                <i id = 'theme-icon' onClick = {toggleMode} className = 'fa-solid fa-moon text-2xl text-accent hover:text-hover dark:text-darkaccent dark:hover:text-darkhover' />
+            </button>
+        </div>
     );
 }
 
